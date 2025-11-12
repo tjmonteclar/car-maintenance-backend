318f5abdac081ee186422317f097a895659fec3c
tjmonteclar <tjmonteclar@gmail.com>
2025-11-12 14:51:15 +0400

    Fixes

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import jsonServer from 'json-server';
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
 
 export default server;
